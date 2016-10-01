9ce0a9640804947d923cc47aabf5d8b0498fe29b
kekemoto <midoaren@gmail.com>
2016-10-01 20:47:11 +0900

    use Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
+var styl = require('gulp-stylus')
 
 //public command
 
 gulp.task('default', ['build'])
 
-gulp.task('build', ['_pug'])
+gulp.task('build', ['_pug', '_styl'])
 
 gulp.task('watch', ['build'], () => {
-    gulp.watch('pug/*.pug', ['_pug']).on('change', (event)=>{
-        console.log('build pug: ' + event.path)
-    })
+    gulp.watch('pug/*.pug', ['_pug'])
+    gulp.watch('stylus/*.styl', ['_styl'])
 })
 
 //private task
@@ -19,4 +19,10 @@ gulp.task('_pug', () => {
     return gulp.src('pug/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./'))
+})
+
+gulp.task('_styl', ()=>{
+    return gulp.src('stylus/*.styl')
+        .pipe(styl())
+        .pipe(gulp.dest('css/'))
 })
