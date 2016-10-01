3bbc649ec1ebe20e7d58089606f70659124eef03
kekemoto <midoaren@gmail.com>
2016-10-01 16:06:15 +0900

    add watch of Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
 
-gulp.task('default', ['pug'])
+//public command
 
-gulp.task('pug', function () {
+gulp.task('default', ['build'])
+
+gulp.task('build', ['_pug'])
+
+gulp.task('watch', ['build'], () => {
+    gulp.watch('pug/*.pug', ['_pug']).on('change', (event)=>{
+        console.log('build pug: ' + event.path)
+    })
+})
+
+//private task
+
+gulp.task('_pug', () => {
     return gulp.src('pug/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./'))
