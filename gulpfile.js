b3ddf01862b9e92c524e3b266f4ea94d17b849f8
kekemoto <midoaren@gmail.com>
2016-10-03 16:40:27 +0900

    add layout.pug
    add nav-box.pug

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,22 @@ gulp.task('default', ['build'])
 gulp.task('build', ['_pug', '_styl'])
 
 gulp.task('watch', ['build'], () => {
-    gulp.watch('pug/*.pug', ['_pug'])
+    gulp.watch('pug/**/*.pug', ['_pug'])
     gulp.watch('stylus/*.styl', ['_styl'])
 })
 
 //private task
 
 gulp.task('_pug', () => {
-    return gulp.src('pug/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('./'))
-})
+        return gulp.src('pug/*.pug')
+            .pipe(pug())
+            .pipe(gulp.dest('./'))
+    }
+)
 
-gulp.task('_styl', ()=>{
-    return gulp.src('stylus/*.styl')
-        .pipe(styl())
-        .pipe(gulp.dest('css/'))
-})
+gulp.task('_styl', () => {
+        return gulp.src('stylus/*.styl')
+            .pipe(styl())
+            .pipe(gulp.dest('css/'))
+    }
+)
